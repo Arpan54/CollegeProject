cfb0ad56ed8c1445efd27f821645992ca9655ac7
Arpan Roy <Arpan>
2024-03-01 00:56:06 +0530

    eighth commit

--- src/components/DropDown/ArtClub.jsx
+++ src/components/DropDown/ArtClub.jsx
@@ -36,18 +36,114 @@ const ArtClub = () => {
               artistic merits of the students who would in turn bring laurels to
               the organization through their constant efforts.
             </p>
-            <p>AESTHETICA would encourage students to express their thoughts, feelings and creativity freely through drawing, sketching and painting. It would further motivate students to actively participate or be directly involved in art competitions. One of the major activities of the club would be to establish art club representatives to help manage and plan activities, and organize group projects. The club would also organize seminar, competitions and workshop for aesthetic enrichment of the students. At the same time the club is also looking forward to organize enrichment outing to a museum, art gallery, or figure drawing session in a year.</p>
+            <p>
+              AESTHETICA would encourage students to express their thoughts,
+              feelings and creativity freely through drawing, sketching and
+              painting. It would further motivate students to actively
+              participate or be directly involved in art competitions. One of
+              the major activities of the club would be to establish art club
+              representatives to help manage and plan activities, and organize
+              group projects. The club would also organize seminar, competitions
+              and workshop for aesthetic enrichment of the students. At the same
+              time the club is also looking forward to organize enrichment
+              outing to a museum, art gallery, or figure drawing session in a
+              year.
+            </p>
           </div>
           <div className="right">
             <h3>Notices</h3>
           </div>
         </SecContainer>
       </Container>
+
+      <Footer>
+        <div className="left">
+          <div className="leftLogo">
+            <img src="../ArtClubLogo.png" alt="" />
+            <div className="text">
+              <h4>AESTHETICA</h4>
+              <h5>The TINT Art Club</h5>
+            </div>
+          </div>
+          <div className="searchBar">
+            <form action="#">
+              <input type="text" />
+              <button className="btn" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, labore! Repellendus doloremque impedit ullam distinctio nihil in eius cumque voluptatum beatae! Explicabo alias id maxime, unde voluptate nam architecto atque!</p>
+        </div>
+      </Footer>
     </>
   );
 };
 
 export default ArtClub;
+
+const Footer = styled.div`
+  position: relative;
+  bottom: 0;
+  background-color: black;
+  height: 400px;
+  width: 100%;
+
+  p{
+    color: white;
+    margin: 0pc 150px 0px 180px;
+    font-size: 12px;
+    width: 285px;
+    text-align: justify;
+  }
+  .searchBar {
+    justify-content: center;
+    margin: 0pc 180px 0px 180px;
+
+    form {
+      align-items: center;
+      input {
+        height: 40px;
+        width: 200px;
+        /* border-radius: 5px; */
+        border: 2px solid #6090D8;
+
+        /* margin-right: 10px; */
+      }
+
+      button {
+        background-color: #6090D8;
+        color: white;
+        padding: 7px 15px 9px 15px;
+        /* border-radius: 5px; */
+        border: 2px solid #6090D8;
+        cursor: pointer;
+        font-weight: 600;
+        font-size: 15px;
+
+        &:hover {
+          background-color: #3a6ebd;
+        }
+      }
+    }
+  }
+  .leftLogo {
+    width: 120px;
+    height: 120px;
+    margin: 0pc 150px 0px 150px;
+    display: flex;
+    align-items: center;
+
+    h4 {
+      color: white;
+      font-size: 25px;
+    }
+    h5 {
+      color: white;
+      font-size: 15px;
+    }
+  }
+`;
 
 const Container = styled.div`
   margin: 0pc 150px 0px 150px;
@@ -70,14 +166,13 @@ const SecContainer = styled.div`
     /* background-color: #b1b1b1; */
 
     border: 5px dashed black;
-    
   }
-  .mid{
+  .mid {
     width: 50%;
-    span{
+    span {
       font-weight: 550;
     }
-    p{
+    p {
       font-weight: 300;
       text-align: justify;
     }
@@ -90,7 +185,7 @@ const SecContainer = styled.div`
     border: 5px dashed black;
   }
 
-  h3{
+  h3 {
     font-size: 30px;
     text-align: center;
     color: #484848;
